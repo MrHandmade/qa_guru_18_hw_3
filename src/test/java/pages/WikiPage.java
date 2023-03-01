4ef72f88657e79c7afbd26bc57c7e27f7734797b
MaximShavikin <shavikin2012@gmail.com>
2023-03-01 18:07:48 +0300

    fix softAssertion block

--- src/test/java/pages/WikiPage.java
+++ src/test/java/pages/WikiPage.java
@@ -1,17 +1,22 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class WikiPage {
-    private static final SelenideElement checkSoftAssertions = $x("//div[@id='wiki-body']");
+    private static final ElementsCollection checkSoftAssertions = $$x("//a[@href='/selenide/selenide/wiki/SoftAssertions']");
     private static final SelenideElement clickSoftAssertions = $x("//a[text()='Soft assertions']");
+    private static final SelenideElement clickShowMorePages = $x("//button[@class='f6 Link--muted js-wiki-more-pages-link btn-link mx-auto']");
 
     public static void checkNGoSoftAssertion(){
-        checkSoftAssertions.shouldHave(Condition.text("Soft assertions")).click();
-        clickSoftAssertions.shouldHave(Condition.text("Soft assertions")).click();
+        clickShowMorePages.click();
+        checkSoftAssertions.first().shouldHave(Condition.text("SoftAssertions"));
+        checkSoftAssertions.last().shouldHave(Condition.text("Soft assertions"));
+        clickSoftAssertions.click();
 
     }
 
